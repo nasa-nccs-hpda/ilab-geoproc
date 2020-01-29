f5cf01b5d3625188e42f8d64440533967978cb3d
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-29 16:46:43 -0500

    Update jian code

--- geoproc/test/jian/landslide1.py
+++ geoproc/test/jian/landslide1.py
@@ -1,6 +1,6 @@
 import xarray as xa
 import os, time
-from geoproc.cluster.manager import ClusterManager
+# from geoproc.cluster.manager import ClusterManager
 
 DATA_DIR = "/att/nobackup/jli30/workspace/landslide/perf_test"
 image_file = os.path.join( DATA_DIR, "4551910_2016-01-02_RE4_3A_Analytic.tif" )
@@ -11,16 +11,27 @@ band_index = 3
 
 if __name__ == '__main__':
 
+    # t0 = time.time()
+    # cluster_parameters = {'type': 'slurm'}
+    # with ClusterManager( cluster_parameters ) as clusterMgr:
+    #
+    #     data_array: xa.DataArray = xa.open_rasterio( image_file, chunks=chunks )
+    #     band_data: xa.DataArray  = data_array.sel( band=band_index, drop=True )
+    #     var_array: xa.DataArray = band_data.coarsen( **block, boundary='pad' ).var()
+    #     var_array.compute().to_netcdf( result_file )
+    #
+    #     print(f" \n Completed operation in {time.time()-t0} seconds, wrote output to {result_file} ")
+
+
     t0 = time.time()
-    cluster_parameters = {'type': 'slurm'}
-    with ClusterManager( cluster_parameters ) as clusterMgr:
 
-        data_array: xa.DataArray = xa.open_rasterio( image_file, chunks=chunks )
-        band_data: xa.DataArray  = data_array.sel( band=band_index, drop=True )
-        var_array: xa.DataArray = band_data.coarsen( **block, boundary='pad' ).var()
-        var_array.compute().to_netcdf( result_file )
+    data_array: xa.DataArray = xa.open_rasterio( image_file )
+    band_data: xa.DataArray  = data_array.sel( band=band_index, drop=True )
+    var_array: xa.DataArray = band_data.coarsen( **block, boundary='pad' ).var()
+    var_array.compute().to_netcdf( result_file )
 
-        print(f" \n Completed operation in {time.time()-t0} seconds, wrote output to {result_file} ")
+    print(f" \n Completed operation in {time.time()-t0} seconds, wrote output to {result_file} ")
+
 
 
 
