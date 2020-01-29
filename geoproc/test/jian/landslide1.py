fbed755ec36cf3e3745859ca4da1021581ac0338
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-29 16:34:41 -0500

    Update jian code

--- geoproc/test/jian/landslide1.py
+++ geoproc/test/jian/landslide1.py
@@ -1,15 +1,16 @@
 import xarray as xa
-import os
+import os, time
 from geoproc.cluster.manager import ClusterManager
 
 DATA_DIR = "/att/nobackup/jli30/workspace/landslide/perf_test"
 image_file = os.path.join( DATA_DIR, "4551910_2016-01-02_RE4_3A_Analytic.tif" )
+result_file = os.path.join( DATA_DIR, "4551910_2016-01-02_RE4_3A_var.nc" )
+block = dict(x=5, y=5)
+band_index = 3
 
 if __name__ == '__main__':
 
-    block = dict( x=5, y=5 )
-    band_index = 3
-
+    t0 = time.time()
     cluster_parameters = {'type': 'slurm'}
     with ClusterManager( cluster_parameters ) as clusterMgr:
 
@@ -20,11 +21,15 @@ if __name__ == '__main__':
         print( band_data.dims )
         print( band_data.shape )
 
-        var_array = data_array.coarsen( **block ).var()
+        var_array: xa.DataArray = band_data.coarsen( **block, boundary='pad' ).var()
 
         print(" \n *Var Data: ")
         print( var_array.dims )
         print( var_array.shape )
+
+        var_array.to_netcdf( result_file )
+
+        print(f" \n Completed operation in {time.time()-t0} seconds, wrote output to {result_file} ")
 
 
 
