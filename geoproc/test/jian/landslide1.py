ac29517ef7c38ced599385a87c3990e06de25244
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-29 16:36:10 -0500

    Update jian code

--- geoproc/test/jian/landslide1.py
+++ geoproc/test/jian/landslide1.py
@@ -4,7 +4,7 @@ from geoproc.cluster.manager import ClusterManager
 
 DATA_DIR = "/att/nobackup/jli30/workspace/landslide/perf_test"
 image_file = os.path.join( DATA_DIR, "4551910_2016-01-02_RE4_3A_Analytic.tif" )
-result_file = os.path.join( DATA_DIR, "4551910_2016-01-02_RE4_3A_var.nc" )
+result_file =  "/tmp/4551910_2016-01-02_RE4_3A_var.nc"
 block = dict(x=5, y=5)
 band_index = 3
 
