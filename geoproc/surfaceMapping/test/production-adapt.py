b47320ae6ece29a9f42dc5c6bc6983ee0e0ce2e4
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-03-19 18:14:11 -0400

    Add Lake mask processing.

---
+++ geoproc/surfaceMapping/test/production-adapt.py
@@ -0,0 +1,11 @@
+import yaml, os
+from geoproc.surfaceMapping.processing import LakeMaskProcessor
+
+CURDIR = os.path.dirname(os.path.abspath(__file__))
+opspec_file = os.path.join(CURDIR, "specs", "lakes2.yml")
+with open(opspec_file) as f:
+    opspecs = yaml.load(f, Loader=yaml.FullLoader)
+    lakeMaskProcessor = LakeMaskProcessor(opspecs)
+    lakeMaskProcessor.process_lakes()
+
+
