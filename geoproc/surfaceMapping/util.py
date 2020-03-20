6b232e44b7430d78a88542fea43fa19c18654728
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-03-20 13:10:45 -0400

    Update lake processing

--- geoproc/surfaceMapping/util.py
+++ geoproc/surfaceMapping/util.py
@@ -30,8 +30,8 @@ class TileLocator:
 
     @classmethod
     def infer_tile_xa( cls, array: xa.DataArray ) -> str:
-        x_coord = array.coords[array.dim[-1]].values
-        y_coord = array.coords[array.dim[-2]].values
+        x_coord = array.coords[array.dims[-1]].values
+        y_coord = array.coords[array.dims[-2]].values
         return cls.get_tile( x_coord[0], x_coord[-1], y_coord[0], y_coord[-1]  )
 
     @classmethod
@@ -57,7 +57,7 @@ class TileLocator:
     def get_bounds(cls, array: xa.DataArray ) -> List:
         print( f"GET BOUNDS, CRS = {array.crs}")
         x_coord = array.coords[array.dims[-1]].values
-        y_coord = array.coords[array.dims[-21]].values
+        y_coord = array.coords[array.dims[-2]].values
         return [ x_coord[0], x_coord[-1], y_coord[0], y_coord[-1] ]
 
 if __name__ == '__main__':
