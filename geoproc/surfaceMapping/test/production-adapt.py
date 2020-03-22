5c993f4a6e2d80153cc48fd1e1ab94f445f08426
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-03-22 12:26:00 -0400

    update test

--- geoproc/surfaceMapping/test/production-adapt.py
+++ geoproc/surfaceMapping/test/production-adapt.py
@@ -2,7 +2,7 @@ import yaml, os
 from geoproc.surfaceMapping.processing import LakeMaskProcessor
 
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-opspec_file = os.path.join(BASEDIR, "specs", "lakes2.yml")
+opspec_file = os.path.join(BASEDIR, "specs", "lakes-test-adapt.yml")
 with open(opspec_file) as f:
     opspecs = yaml.load(f, Loader=yaml.FullLoader)
     lakeMaskProcessor = LakeMaskProcessor(opspecs)
