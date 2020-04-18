c5fcc08f3a369aed1546d77b25a3774abaf7084a
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-04-18 13:11:45 -0400

    Add Aviris file support

--- geoproc/umap/validate_data.py
+++ geoproc/umap/validate_data.py
@@ -1,10 +1,13 @@
 import xarray as xa
 import numpy as np
 from typing import List, Union, Tuple, Optional
+import sys
 import os, math
 
-DATA_DIR = "/Users/tpmaxwel/Dropbox/Tom/Data/Aviris/processed/"
-aviris_tile = "ang20170701t182520"
+if len(sys.argv) < 3: print( "Usage: validate_data <data_location> <tile_id>")
+
+DATA_DIR = sys.argv[1]   # "/Users/tpmaxwel/Dropbox/Tom/Data/Aviris/processed/"
+aviris_tile = sys.argv[2]   # "ang20170701t182520"
 nodata_value = -9999
 
 input_file = os.path.join( DATA_DIR, f"{aviris_tile}_rfl_v2p9", f"{aviris_tile}_corr_v2p9.tif" )
