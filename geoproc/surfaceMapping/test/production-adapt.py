a86b070c3507b013497de4b1bcec0df68e63c537
Thomas Maxwell <thomas.maxwell@nasa.gov>
2020-06-23 11:55:05 -0400

    Skip processed files

--- geoproc/surfaceMapping/test/production-adapt.py
+++ geoproc/surfaceMapping/test/production-adapt.py
@@ -4,9 +4,10 @@ from geoproc.surfaceMapping.processing import LakeMaskProcessor
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 opspec_file = os.path.join(BASEDIR, "specs", "lakes-test-adapt.yml")
 reproject_inputs = False
+skip_existing =  False
 with open(opspec_file) as f:
     opspecs = yaml.load(f, Loader=yaml.FullLoader)
     lakeMaskProcessor = LakeMaskProcessor(opspecs)
-    results = lakeMaskProcessor.process_lakes( reproject_inputs, format="tif" )
+    results = lakeMaskProcessor.process_lakes( reproject_inputs, format="tif", skip_existing=skip_existing )
 
 
