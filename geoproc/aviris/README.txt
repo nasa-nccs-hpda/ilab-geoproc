dba5acdcf9069f5b4d21b4734196b10891b8389c
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2022-03-23 16:45:16 -0400

    Changes to support new version of AVIRIS regridding

--- geoproc/aviris/README.txt
+++ geoproc/aviris/README.txt
@@ -1,6 +1,81 @@
-Sample execution of regridder script:
+## Sample execution of regridder script:
 
+```bash
 >> cd /att/pubrepo/ILAB/projects/geoProc/
-
 >> ./geoproc/aviris/regridder.py  '/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/ang201707*rfl/ang201707*_rfl_v2p9/ang201707*_v2p9_img' /css/above/AVIRIS_Analysis_Ready
+```
 
+## Things to check
+
+- Is it faster if the environment is in another location (not anaconda)
+- Where to do the globbing of files?
+- Can we speed up gdal warp with multiprocessing?
+- Is multiprocessing actually working?
+- What about a system with more resources?
+
+## Number of Upper Directories Missing
+
+```bash
+Wed Mar 23 09:21:31 EDT 2022
+[jacaraba@abovex101 system]$ ls -d /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/*rfl | wc
+    847     847   77924
+[jacaraba@abovex101 system]$ ls -d /css/above/AVIRIS_Analysis_Ready/*rfl | wc
+    437     437   24035
+```
+
+## Benchmarking Regridder with new updates
+
+Command for testing
+
+```bash
+python regridder.py -f '/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170628t165756rfl/ang20170628t165756_rfl_v2p9/*img' -o /css/above/AVIRIS_Analysis_Ready/AVIRIS_Analysis_Ready_Test
+```
+
+## Testing Regridder
+
+```bash
+python regridder.py -f '/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang201707*rfl/ang201707*_rfl_v2p9/ang201707*_v2p9_img' /css/above/AVIRIS_Analysis_Ready
+```
+
+```bash
+[jacaraba@abovex101 system]$ ls /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang201707*rfl/ang201707*_rfl_v2p9/ang201707*_v2p9_img | more
+/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170701t182520rfl/ang20170701t182520_rfl_v2p9/ang20170701t182520_corr_v2p9_img
+/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170701t182520rfl/ang20170701t182520_rfl_v2p9/ang20170701t182520_h2o_v2p9_img
+/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170701t183738rfl/ang20170701t183738_rfl_v2p9/ang20170701t183738_corr_v2p9_img
+/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170701t183738rfl/ang20170701t183738_rfl_v2p9/ang20170701t183738_h2o_v2p9_img
+...
+```
+
+## New version testing
+
+```bash
+python regridder.py -f /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/*rfl -o /adapt/nobackup/projects/ilab/data/Aviris/AvirisAnalysisReady
+```
+
+
+
+(ilab) [jacaraba@ilab112 aviris]$ python regridder.py -f '/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang201707*rfl/ang201707*_rfl_v2p9/ang201707*_v2p9_img' -o /adapt/nobackup/projects/ilab/data/Aviris/AvirisAnalysisReady
+connect localhost port 6026: Connection refused
+2022-03-23 10:04:00,512 Found 2 total raw files.
+IN_FILE  /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170731t222832rfl/ang20170731t222832_rfl_v2p9 ang20170731t222832_corr_v2p9_img
+IN_DIR  /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170731t222832rfl/ang20170731t222832_rfl_v2p9 /adapt/nobackup/projects/ilab/data/Aviris/AvirisAnalysisReady/ang20170731t222832rfl/ang20170731t222832_rfl_v2p9 ang20170731t222832_corr_v2p9.tif
+IN_FILE  /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170731t222832rfl/ang20170731t222832_rfl_v2p9 ang20170731t222832_h2o_v2p9_img
+IN_DIR  /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/ang20170731t222832rfl/ang20170731t222832_rfl_v2p9 /adapt/nobackup/projects/ilab/data/Aviris/AvirisAnalysisReady/ang20170731t222832rfl/ang20170731t222832_rfl_v2p9 ang20170731t222832_h2o_v2p9.tif
+2022-03-23 10:04:00,528 Took 0.19 min, output at /adapt/nobackup/projects/ilab/data/Aviris/AvirisAnalysisReady.
+
+
+## This is what we really need
+
+(ilab) [jacaraba@ilab112 aviris]$ ls /css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/*rfl/*_rfl_*/*_*_img | wc
+   1694    1694  258335
+
+(ilab) [jacaraba@ilab112 aviris]$ ls /css/above/AVIRIS_Analysis_Ready/*rfl/*_rfl_*/*_*.tif | wc
+    850     850   98175
+
+## Processing Steps
+
+module load singularity
+singularity exec -B /adapt/nobackup/people/jacaraba,/adapt/nobackup/projects/ilab,/css/above /adapt/nobackup/projects/ilab/containers/ilab-base_gdal-3.3.3.sif python /adapt/nobackup/people/jacaraba/development/geoProc/geoproc/aviris/regridder.py -f '/css/above/daac.ornl.gov/daacdata/above/ABoVE_Airborne_AVIRIS_NG/data/*rfl/*_rfl_*/*_*_img' -o /css/above/AVIRIS_Analysis_Ready -to /adapt/nobackup/projects/ilab/data/Aviris/AvirisAnalysisReady
+
+
+singularity shell -B /css,/adapt/nobackup/people/jacaraba,/adapt/nobackup/projects/ilab /adapt/nobackup/projects/ilab/containers/ilab-base_gdal-3.3.3.sif
--- geoproc/aviris/regridder.py
+++ geoproc/aviris/regridder.py
@@ -19,80 +19,135 @@ globallock = Lock()
 
 class AvirisWarp:
 
-    def __init__(self, output_dir: str) -> None:
+    def __init__(self, output_dir: str, temporary_output_dir: str = None) -> None:
 
+        # main output directory, it is normally a CSS directory
         self.output_dir = output_dir
 
-    """
-    def needs_processing(self, output_file ) -> bool:
+        # sometimes we need to output to other directories, but need to retain
+        # which files have been processed on the main output_dir to not
+        # repeat processing and waste resources
+        self.temporary_output_dir = temporary_output_dir
+
+    def get_file_paths(self, input_file: str) -> Tuple[str,str,str]:
+        """
+        Get file paths of the files extracted from raw data.
+        """
+        infile_dir, infile_name = os.path.split(input_file)
+        base1, dir1 = os.path.split(infile_dir)
+        base0, dir0 = os.path.split(base1)
+        ofile_name = infile_name[:-4] if infile_name.endswith("_img") else infile_name
+        ifile_outputdir = os.path.join(self.output_dir, dir0, dir1)
+        return  infile_dir, ifile_outputdir, ofile_name + ".tif"
+
+    def get_unprocessed_filepaths(self, files_glob: str) -> List[str]:
+        """
+        Get a filtered list of unprocessed files.
+        """
+        files_list = glob(files_glob)
+        filtered_file_list = []
+        logging.info(f'Found {len(files_list)} total raw files.')
+
+        for input_file in files_list:
+            input_dir, output_dir, output_file = self.get_file_paths(input_file)
+            output_file_path = os.path.join(output_dir, output_file)
+            if self.needs_processing(output_file_path):
+                filtered_file_list.append( input_file )
+        
+        logging.info(f'Found {len(filtered_file_list)} files that need processing.')
+        return filtered_file_list
+
+    def process_files(self, files_glob: str, num_procs: int = cpu_count()) -> None:
+        """
+        Process multiple files via multiprocessing.
+        """
+        files_list = self.get_unprocessed_filepaths(files_glob)[:10]
+        logging.info(f'Using {num_procs} processors for {len(files_list)} files from {files_glob}')
+        
+        p = Pool(processes=num_procs)
+        p.map(self.process_file, files_list)
+        p.close()
+        p.join()
+        return
+
+    def needs_processing(self, output_file) -> bool:
+        """
+        Validate if file needs processing and is correctly formatted.
+        """
         try:
-            test_data = xa.open_rasterio(output_file)
+            test_data = rxr.open_rasterio(output_file)
         except:
             if os.path.isfile(output_file): os.remove(output_file)
             return True
         return False
 
-    def get_unprocessed_filepaths(self, files_glob: str ) -> List[str]:
-        filelist = glob( files_glob )
-        filtered_file_list = []
-        for input_file in filelist:
-            input_dir, output_dir, output_file = self.get_file_paths(input_file)
-            output_file_path = os.path.join(output_dir, output_file)
-            if self.needs_processing(output_file_path):
-                filtered_file_list.append( input_file )
-        print( f"Processing {len(filtered_file_list)} files out of {len(filelist)}, files requiring processing = {[os.path.basename(f) for f in filelist]}")
-        return filtered_file_list
-
-    def process_files( self, files_glob: str, **kwargs ):
-        files_list = self.get_unprocessed_filepaths( files_glob )
-        nproc = kwargs.get('np', cpu_count() )
-        print( f"Using {nproc} processors to process {len(files_list)} files from the glob '{files_glob}'")
-        p = Pool( processes=nproc )
-        p.map( self.process_file, files_list )
-        p.close()
-        p.join()
-
-    def process_file( self, input_file: str ):
+    def process_file(self, input_file: str) -> None:
+        """
+        Process single filename.
+        """
+        # gather the file paths
         input_dir, output_dir, output_file = self.get_file_paths(input_file)
-        output_file_path = os.path.join( output_dir, output_file )
-        if not self.needs_processing( output_file_path ):
+
+        print("input_dir", input_dir)
+
+        # select the output directory based on the available output_dirs
+        if self.temporary_output_dir is not None:
+            output_dir = os.path.join(
+                self.temporary_output_dir,
+                os.path.basename(os.path.dirname(output_dir)),
+                os.path.basename(output_dir)
+            )
+
+        os.makedirs(output_dir, mode=0o777, exist_ok=True)
+        output_file_path = os.path.join(output_dir, output_file)
+
+        # lock file for multi-node, multi-processing
+        lock_filename = f'{output_file_path}.lock'
+
+        # Skip this process if lock file exist
+        if os.path.isfile(lock_filename) and \
+            not self.needs_processing(output_file_path):
             globallock.acquire()
-            print( f"Skipping file {input_file}: Processed file already exists at {output_file_path}")
+            logging.info(f'Skipping {input_file}: File exists at {output_file_path}')
             globallock.release()
+            return
+
+        # Open lock filename
+        open(lock_filename, 'w').close()
+
+        globallock.acquire()
+        logging.info(f'Processing file {input_file}')
+        globallock.release()
+        
+        self.copy_files(f"{input_dir}/*README*", output_dir)
+        t0 = time.time()
+
+        args = [ 
+            'gdalwarp', '-co', 'COMPRESS=LZW', '-co', 'BIGTIFF=YES',
+            input_file, output_file_path
+        ]
+        rv = subprocess.call(args)
+
+        globallock.acquire()
+        if rv == 0:
+            logging.info(f'File {output_file} generated in {(time.time()-t0)/60.0:.2f} minutes.')
         else:
-            globallock.acquire()
-            print(f"Processing file '{input_file}'")
-            globallock.release()
+            logging.info(f'Error when processing file {input_file}')
+        globallock.release()
+        return
 
-            self.copy_files( f"input_dir/*README*", output_dir )
-            t0 = time.time()
-
-            args = [ 'gdalwarp', '-co', 'COMPRESS=LZW', '-co', 'BIGTIFF=YES', input_file, output_file_path ]
-            rv = subprocess.call(args)
-
-            globallock.acquire()
-            if rv == 0:    print( f"File '{output_file}' generated in {(time.time()-t0)/60.0:.2f} minutes in output-dir {output_dir}." )
-            else:          print( f"Error when processing file '{input_file}'" )
-            globallock.release()
-
-    def get_file_paths( self, input_file: str ) -> Tuple[str,str,str]:
-        infile_dir, infile_name = os.path.split(input_file)
-        base1, dir1 = os.path.split(infile_dir)
-        base0, dir0 = os.path.split(base1)
-        ofile_name = infile_name[:-4] if infile_name.endswith("_img") else infile_name
-        ifile_outputdir = os.path.join( self.outputDir, dir0, dir1 )
-        os.makedirs( ifile_outputdir, mode=0o777, exist_ok=True )
-        return  infile_dir, ifile_outputdir, ofile_name + ".tif"
-
-    def copy_files(self, files_glob, output_dir ):
+    def copy_files(self, files_glob: str, output_dir: str):
+        """
+        Copy files from raw data directory to output directory.
+        """
         files_list = glob(files_glob)
         for ifile in files_list:
-            infile_dir, infile_name = os.path.split( ifile )
-            ofile = os.path.join( output_dir, infile_name )
-            if not os.path.isfile( ofile ):
-                print( f"Copying file {infile_name} to dir {output_dir}")
-                shutil.copyfile( ifile, ofile )
-    """
+            infile_dir, infile_name = os.path.split(ifile)
+            ofile = os.path.join(output_dir, infile_name)
+            if not os.path.isfile(ofile):
+                logging.info(f'Copying file {infile_name} to dir {output_dir}')
+                shutil.copyfile(ifile, ofile)
+        return
 
 
 def main():
@@ -117,6 +172,15 @@ def main():
                         help='Output directory to store analysis ready data.')
 
     parser.add_argument(
+                        '-to',
+                        '--temporary-output-dir',
+                        type=str,
+                        required=False,
+                        default=None,
+                        dest='temporary_output_dir',
+                        help='Temporary output directory to store analysis ready data.')
+
+    parser.add_argument(
                         '-n',
                         '--num-procs',
                         type=str,
@@ -127,13 +191,14 @@ def main():
 
     args = parser.parse_args()
 
-    awarp = AvirisWarp(args.output_dir)
+    # Set logging
+    logging.basicConfig(format='%(asctime)s %(message)s', level='INFO')
+    timer = time.time()
 
-    """
-    awarp = AvirisWarp( output_dir )
-    awarp.process_files( files_glob.replace('"', ''), **kwargs )
-    print( f"Files processed in {(time.time()-t0)/60.0:.2f} minutes to output-dir {output_dir}.")
-    """
+    awarp = AvirisWarp(args.output_dir, args.temporary_output_dir)
+    awarp.process_files(args.files_glob.replace('"', ''))
+    logging.info(
+        f'Took {(time.time()-timer)/60.0:.2f} min, output at {args.output_dir}.')
 
 
 # -----------------------------------------------------------------------------
